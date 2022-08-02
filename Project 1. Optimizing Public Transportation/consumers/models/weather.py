4fcb21e218422a25f6faf78857dc1727232c15ee
InesArrom <60150449+InesArrom@users.noreply.github.com>
2022-08-02 13:24:04 +0200

    project1.v1

--- Project 1. Optimizing Public Transportation/consumers/models/weather.py
+++ Project 1. Optimizing Public Transportation/consumers/models/weather.py
@@ -15,9 +15,7 @@ class Weather:
 
     def process_message(self, message):
         """Handles incoming weather data"""
-        logger.info("weather process_message is incomplete - skipping")
-        #
-        #
-        # TODO: Process incoming weather messages. Set the temperature and status.
-        #
-        #
+        # Process incoming weather messages. Set the temperature and status.
+        msg_value = message.value()
+        self.temperature = msg_value.get('temperature')
+        self.status = msg_value.get('status')
--- Project 1. Optimizing Public Transportation/producers/models/producer.py
+++ Project 1. Optimizing Public Transportation/producers/models/producer.py
@@ -9,6 +9,10 @@ from confluent_kafka.avro import AvroProducer
 
 logger = logging.getLogger(__name__)
 
+# Broker properies Host URL
+BROKER_URL = "PLAINTEXT://localhost:9092" # PLAINTEXT://localhost:9093,PLAINTEXT://localhost:9094"
+SCHEMA_REGISTRY_URL = "http://localhost:8081"
+
 
 class Producer:
     """Defines and provides common functionality amongst Producers"""
@@ -31,16 +35,11 @@ class Producer:
         self.num_partitions = num_partitions
         self.num_replicas = num_replicas
 
-        #
-        #
-        # TODO: Configure the broker properties below. Make sure to reference the project README
+        # Configure the broker properties below. Make sure to reference the project README
         # and use the Host URL for Kafka and Schema Registry!
-        #
-        #
         self.broker_properties = {
-            # TODO
-            # TODO
-            # TODO
+            'bootstrap.servers': BROKER_URL,
+            'schema.registry.url': SCHEMA_REGISTRY_URL
         }
 
         # If the topic does not already exist, try to create it
@@ -48,31 +47,57 @@ class Producer:
             self.create_topic()
             Producer.existing_topics.add(self.topic_name)
 
-        # TODO: Configure the AvroProducer
-        # self.producer = AvroProducer(
-        # )
+        # Configure the AvroProducer
+        self.producer = AvroProducer(
+            config=self.broker_properties,
+            default_key_schema=self.key_schema,
+            default_value_schema=self.value_schema
+        )
 
     def create_topic(self):
         """Creates the producer topic if it does not already exist"""
-        #
-        #
-        # TODO: Write code that creates the topic for this producer if it does not already exist on
+
+        # Write code that creates the topic for this producer if it does not already exist on
         # the Kafka Broker.
-        #
-        #
-        logger.info("topic creation kafka integration incomplete - skipping")
+
+        # Admin client for kafka broker
+        client = AdminClient(
+            {"bootstrap.servers": self.broker_properties['bootstrap.servers']}
+        )
+
+        # Check the topics map in the kafka information
+        if self.topic_name not in client.list_topics().topics:
+            logger.info(f"Creating topic {self.topic_name}")
+
+            topic_futures = client.create_topics([
+                NewTopic(
+                    topic=self.topic_name,
+                    num_partitions=self.num_partitions,
+                    replication_factor=self.num_replicas
+                )
+            ])
+
+            for topic, future in topic_futures.items():
+                try:
+                    future.result()
+                    logger.info(f"Topic {topic} created")
+                except Exception as e:
+                    logger.info(f"Error to create topic {topic}: {e}")
+
+        else:
+            logger.info(f"Topic {self.topic_name} already exists")
 
     def time_millis(self):
         return int(round(time.time() * 1000))
 
     def close(self):
         """Prepares the producer for exit by cleaning up the producer"""
-        #
-        #
+        
         # TODO: Write cleanup code for the Producer here
-        #
-        #
-        logger.info("producer close incomplete - skipping")
+        if self.producer:
+            self.producer.flush()
+
+        logger.info("Producer closed")
 
     def time_millis(self):
         """Use this function to get the key for Kafka Events"""
